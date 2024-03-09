3f9b5e1a50b6670793ab8a82b4b634095cdf0566
Emily <140002969+AngelBaee@users.noreply.github.com>
2024-03-09 17:39:11 +0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+def post_list(request):
+    return render(request, 'blog/post_list.html', {'C:\Users\99293\djangogirl\blog\templates\blog\post2_list.html'})
+
+
